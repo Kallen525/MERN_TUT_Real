85b65b245f82f291c0af4e437a916c812fe41299
Kallen525 <kallenwork1@gmail.com>
2024-09-01 15:01:46 -0500

    Have the posting feature working in full

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,3 +1,10 @@
+import { Link } from "react-router-dom";
+
 export function About() {
-  return <>About page</>;
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Hello this is a fake page I am just doing this for my mind!</p>
+    </div>
+  );
 }
